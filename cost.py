500402c46279f00e653943fd8065c9e0ad91a0de
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-02-08 16:40:05 -0700

    costdistance

--- cost.py
+++ cost.py
@@ -1,3 +1,5 @@
+import math
+
 class CostDistance:
 """
     A class with a method called distance that will return the Euclidean
@@ -9,9 +11,17 @@ def __init__(self, location):
         Creates an instance of the CostDistance class and stores the
         dictionary "location" as a member of this class.
         """
-        
+
+        self.locDict = location
+
 def distance(self, e):
         """
         Here e is a pair (u,v) of vertices.
         Returns the Euclidean distance between the two vertices u and v.
         """
+
+        u, v = e
+        lon1, lat1 = self.location[u]
+        lon2, lat2 = self.location[v]
+        dist = math.sqrt( (lat2 - lat1)**2 + (lon2 - lon1)**2 )
+        return int(dist)
