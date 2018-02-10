a1d9e72efde791f3213f8ef115dce00d241e2790
Jacob Wong <jdw2@ualberta.ca>
2018-02-09 17:13:36 -0700

    stuff

--- server.py
+++ server.py
@@ -7,9 +7,13 @@ from dijkstra import least_cost_path
 from build import load_edmonton_graph
 from cost import CostDistance
 
+yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
+# location is a dict where keys are the vertices and holds a tuple of the coordinates
+
 request = input().strip().split(" ")
 print(request)
 valid = False
+vertices = yegGraph.getVertices()
 
 if request[0] == 'R':
     valid = True
@@ -17,12 +21,7 @@ else:
     print('invalid request')
 
 
-#def nearest_vertex():
-
-#def output():
-
-#def communicate():
 
 if __name__ == "__main__":
     # Code for processing route finding requests here
-    yegGraph = load_edmonton_graph('edmonton-roads-2.0.1.txt')
+    yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
