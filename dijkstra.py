4a83b787a4924fdb36e8431425b1da2c522f9bfd
Jacob Wong <jdw2@ualberta.ca>
2018-02-09 18:44:13 -0700

    stuff

--- dijkstra.py
+++ dijkstra.py
@@ -29,12 +29,12 @@ def least_cost_path(graph, start, dest, cost):
     events.insert((start, start), 0) # vertex s burns at time 0
 
 
-    while events.__len__() > 0:
+    while len(events) > 0:
         vertices, time = events.popmin()
         if vertices[1] not in reached:
             reached[vertices[1]] = vertices[0]# vertices[0] = reached[vertices[1]]   burn vertex v, record predecessor u
             for n in graph.neighbours(vertices[1]):  # new event: edge (v,w) started burning
-                events.insert(([vertices[1]], n), time + cost.distance((vertices[1], n)))
+                events.insert(([vertices[1]], n), time + cost.distance(vertices[1], n))
 
             if vertices[1] == dest:
                 break
