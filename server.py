401f863547c3aa6424a3b8f7fc080d747c60c7f2
Jacob Wong <jdw2@ualberta.ca>
2018-02-09 17:19:03 -0700

    test

--- server.py
+++ server.py
@@ -10,18 +10,20 @@ from cost import CostDistance
 yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
 # location is a dict where keys are the vertices and holds a tuple of the coordinates
 
-request = input().strip().split(" ")
-print(request)
-valid = False
-vertices = yegGraph.getVertices()
 
-if request[0] == 'R':
-    valid = True
-else:
-    print('invalid request')
 
 
 
 if __name__ == "__main__":
     # Code for processing route finding requests here
     yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
+
+    request = input().strip().split(" ")
+    valid = False
+    vertices = yegGraph.getVertices()
+    print(vertices)
+
+    if request[0] == 'R':
+        valid = True
+    else:
+        print('invalid request')
