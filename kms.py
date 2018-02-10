3f7f2c6dad5dce7d9ff54cd302829e26e56fe8a7
raysarinas <rsarinas@ualberta.ca>
2018-02-10 16:31:03 -0700

    😂 <- zac laughing @ my pain

--- kms.py
+++ kms.py
@@ -65,36 +65,39 @@ def checkinput():
 ''' ------------------------------------ '''
 ''' ------------------------------------ '''
 
-yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
-cost = CostDistance(location)
-request = input().strip().split(" ")
-valid = request[0]
-startcoord = [int(request[1]), int(request[2])]
-destination = [int(request[3]), int(request[4])]
-# print(startcoord, destination)
-start = None
-end = None
+if __name__ == "__main__":
+    yegGraph, location = load_edmonton_graph('edmonton-roads-2.0.1.txt')
+    cost = CostDistance(location)
+    request = input().strip().split(" ")
+    valid = request[0]
+    startcoord = [int(request[1]), int(request[2])]
+    destination = [int(request[3]), int(request[4])]
+    # print(startcoord, destination)
+    start = None
+    end = None
 
-for vertex, point in location.items():
-    if point[0] == startcoord[0] and point[1] == startcoord[1]:
-        start = vertex
-    if point[0] == destination[0] and point[1] == destination[1]:
-        end = vertex
+    for vertex, point in location.items():
+        if point[0] == startcoord[0] and point[1] == startcoord[1]:
+            start = vertex
+        if point[0] == destination[0] and point[1] == destination[1]:
+            end = vertex
 
-if start == None or end == None:
-    print("god is dead")
-# else:
-#     print(start, end)
+    if start == None or end == None:
+        print("god is dead")
+        print("also can't find whatever input that was inputted probably")
+    # else:
+    #     print(start, end)
 
-reached = least_cost_path(yegGraph, start, end, cost)
-''' SOMEONE FIX THIS PLEASE THANKS '''
-''' LEAST COST PATH KEEPS RETURNING 57779 IDK WHY '''
-waypoints = len(reached)
-print(reached)
-print('N', waypoints)
+    reached = least_cost_path(yegGraph, start, end, cost)
+    ''' SOMEONE FIX THIS PLEASE THANKS '''
+    ''' LEAST COST PATH KEEPS RETURNING 57779 IDK WHY '''
+    ''' DO WE NEED TO FIND THE CLOSEST VERTEX? IM CONFUSED LIKE IDK WHATS HAPPENING '''
+    waypoints = len(reached)
+    print(reached)
+    print('N', waypoints)
 
-for path in reached:
-    (lat, lon) = location[path]
-    checkinput()
+    for path in reached:
+        (lat, lon) = location[path]
+        checkinput()
 
-print('E')
+    print('E')
