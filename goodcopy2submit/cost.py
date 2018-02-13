8aed04c402c39da725159432091172bbcb946047
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-02-12 17:08:24 -0700

    commentos

--- goodcopy2submit/cost.py
+++ goodcopy2submit/cost.py
@@ -1,11 +1,24 @@
 import math
 
 class CostDistance():
+    """
+    Creates an instance of the CostDistance class and stores the
+    dictionary "location" as a member of this class.
+    """
     def __init__(self, location):
+        """
+        Creates an instance of the CostDistance class and stores the
+        dictionary "location" as a member of this class.
+        """
         self.locDict = location
 
     def distance(self, e):
+        """
+        Here e is a pair (u,v) of vertices.
+        Returns the Euclidean distance between the two vertices u and v.
+        """
         lon1, lat1 = self.locDict[e[0]]
         lon2, lat2 = self.locDict[e[1]]
+        #calculates the Euclidean distance between two points(x1,y1) and (x2,y2)
         e_dist = math.sqrt((lat2 - lat1) ** 2 + (lon2 - lon1) ** 2)
         return e_dist
--- goodcopy2submit/dijkstra.py
+++ goodcopy2submit/dijkstra.py
@@ -2,6 +2,21 @@ from graph import Graph
 from binary_heap import BinaryHeap
 
 def least_cost_path(graph, start, dest, cost):
+    """
+    Loads the graph of Edmonton from the given file.
+    Returns two items
+    graph: the instance of the class Graph() corresponding to the
+    directed graph from edmonton-roads-2.0.1.txt
+    location: a dictionary mapping the identifier of a vertex to
+    the pair (lat, lon) of geographic coordinates for that vertex.
+    These should be integers measuring the lat/lon in 100000-ths
+    of a degree.
+    In particular, the return statement in your code should be
+    return graph, location
+    (or whatever name you use for the variables).
+    Note: the vertex identifiers should be converted to integers
+    before being added to the graph and the dictionary.
+    """
     reached = {} # empty dictionary
     events = BinaryHeap() # empty heap
     events.insert((start, start), 0) # vertex s burns at time 0
@@ -9,12 +24,14 @@ def least_cost_path(graph, start, dest, cost):
     while len(events) > 0:
         edge, time = events.popmin()
         if edge[1] not in reached:
-            reached[edge[1]] = edge[0]
+            reached[edge[1]] = edge[0] # burn vertex v, record predecessor u
             for nbr in graph.neighbours(edge[1]):
                 events.insert((edge[1], nbr), time + cost.distance((edge[1], nbr)))
 
+    # if the dest is not in the reached dictionary, then return an empty list
     if dest not in reached:
       return []
+
 
     current = dest
     path = [current]
