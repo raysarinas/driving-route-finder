2fffa4beb19695c1dc177c2f81b3bb0caf21cdd4
raysarinas <rsarinas@ualberta.ca>
2018-03-02 17:40:59 -0700

    jk here it is now i guess

--- part2/client.cpp
+++ part2/client.cpp
@@ -91,6 +91,165 @@ void process_input() {
   }
 }
 
+void talk(lon_lat_32 start, lon_lat_32 end) {
+  // this fun stuff is what allows for communication between
+  // the arduino/client and python server to happen I guess
+
+  // state machine unfortunately
+  enum State {Begin, Wait4N, Wait4W, Done};
+  // dont need SendA state?
+
+  State current_state = Begin;
+  Serial.println("beginning/starting whatever u wanna call it");
+
+  // variables apparently
+  int32_t n_waypoints;
+  int32_t waypoint;
+  bool got_waypoints;
+  int timeout;
+  char incoming;
+  bool check1 = true;
+  bool check2 = false;
+
+
+  int path = 0;
+  int32_t W_lat_long = 0;
+  int count = 1;
+
+
+  while (true) { // infinite loopy loop
+    while (Serial.available() == 0);
+
+    if (current_state == Begin) {
+      // send the start and end waypoints?
+      Serial.print("R ");
+      Serial.print(start.lat);
+      Serial.print(" ");
+      Serial.print(start.lon);
+      Serial.print(" ");
+      Serial.print(end.lat);
+      Serial.print(" ");
+      Serial.println(end.lon);
+      Serial.flush();
+      current_state = Wait4N;
+      timeout = millis();
+      // move onto next state? idk why
+    }
+
+    if (current_state == Wait4N) {
+      n_waypoints = 0;
+      // set to zero just in case
+
+      while (true) {
+        // read incoming bytes from server once we get '\n'
+        // so we know the number of waypoints / information have been stored
+        // something something idk
+        incoming = Serial.read(); // read the incoming byte(s)
+
+        // can we use a SWITCH thing to make this better or nah?
+        if (incoming == (-1 || '%' || ' ')) {
+          continue; // if nothing in serial read
+          // or if handshaking is weird or to skip over spaces
+        }
+
+        if (incoming == 'N') {
+          got_waypoints = true; // make sure got num_waypoints
+          continue;
+        }
+
+        if (incoming == '\n') {
+          // end of the line for incoming data so need to send A
+
+          if (got_waypoints) {
+
+            if ((n_waypoints != 0) && n_waypoints <= 499) {
+              current_state = Wait4W; // if range of waypoints O.K. continue to next state
+              shared.num_waypoints = n_waypoints; // get the 'N' thing from server i think?
+              Serial.write('A');
+              got_waypoints = false; // reset the checker thingy so doesn't mess up
+            }
+
+            if (n_waypoints > 499) { // too many paths, then treat as no waypoints/path
+              Serial.write('A');
+              current_state = Done;
+              break;
+            }
+
+            else { // if N == 0 or > 499
+              Serial.write('A')
+              current_state = Done;
+
+              if (n_waypoints == 0) {
+                n_waypoints = 0;
+                got_waypoints = false;
+              }
+              break;
+            } // end of else
+
+          }
+          break; // IDK WHY THO
+        }
+
+        // n_waypoints *= 10;
+        // n_waypoints += (incoming - 48);
+        n_waypoints = (n_waypoints*10) + incoming - 48; // IDK WHAT THIS IS
+
+        if ((millis() - timeout) > 10000) {
+          current_state = Done; // time out I guess but idk when it would
+        }
+      }
+    }
+
+    if (current_state == Wait4W) {
+      while (true) { // anotha one so know to store all crap?
+        incomingByte = Serial.read();
+
+        if (incoming == (-1 || '%' || '-')) {
+          continue; // if nothing in serial read
+          // or if handshaking is weird or to skip over spaces
+        }
+
+        if (incoming == 'W') { // DO WE NEED THIS CHECK????
+          // make sure got data
+          got_waypoints = true;
+          continue;
+        }
+
+        if ((incomingByte == ' ') && (check1)) {
+          // we ignore the first space and mark it with count that it has happened
+          check1 = false;
+          check2 = true;
+          continue;
+        }
+        if ((incomingByte == ' ') && (check2) && (check)) {
+          // this is the second space and marks that we have all the latitude
+          // values this waypoint has. we store it and clear our temp out
+          check1 = false;
+          check2 = false;
+          shared.waypoints[path].lat = W_lat_long;
+          waypoint = 0;
+          continue;
+        }
+
+        // something something numeric value crap
+        waypoint = (waypoint*10) + incoming - 48;
+
+
+      }
+    }
+
+    ''' might be able to remove this because redundant'''
+    if (current_state == Done) {
+      // if communication done break loop and draw lines
+      // and whatever other crap apparently
+      // APPARENTLY THIS MIGHT BE REDUNDANT CODE BUT WHOMSDT KNOWS
+      Serial.flush();
+      break;
+    }
+  }
+
+}
+
 int main() {
   setup();
 
@@ -136,7 +295,18 @@ int main() {
         start = get_cursor_lonlat();
         curr_mode = WAIT_FOR_STOP;
         status_message("TO?");
-        ''' add Serial print stuff apparently? maybe 4 testing '''
+
+        //******************* SERIAL MON TEST:
+        Serial.print("START POINT: ")
+        Serial.print(start.lon);
+        Serial.print(", ");
+        Serial.println(start.lat);
+        Serial.print("START POINT COORDS ON MAP COORDS");
+        Serial.print(longitude_to_x(shared.map_number, start.lon));
+        Serial.print(", ");
+        Serial.println(latitude_to_y(shared.map_number,start.lat));
+        //******************** END
+
 
         // wait until the joystick button is no longer pushed
         while (digitalRead(clientpins::joy_button_pin) == LOW) {}
@@ -146,15 +316,19 @@ int main() {
         // and then communicate with the server to get the path
         end = get_cursor_lonlat();
 
+        //******************* SERIAL MON TEST:
+        Serial.print("ENDPOINT: ")
+        Serial.print(end.lon);
+        Serial.print(", ");
+        Serial.println(end.lat);
+        Serial.print("ENDPOINT COORDS ON MAP COORDS");
+        Serial.print(longitude_to_x(shared.map_number, end.lon));
+        Serial.print(", ");
+        Serial.println(latitude_to_y(shared.map_number, end.lat));
+        //******************** END
+
         // TODO: communicate with the server to get the waypoints
-        ''' serial print stuff for testing i guess
-
-            status message? wtf
-         '''
-        communicate(start, end);
-
-
-        ''' a lot of crap '''
+        talk(start, end);
 
         // now we have stored the path length in
         // shared.num_waypoints and the waypoints themselves in
